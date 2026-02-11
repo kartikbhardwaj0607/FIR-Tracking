c7337539ffead9212979e7dba2443e244a1d32b8
Kartik Sharma <kartiksharma0607@gmail.com>
2026-02-11 21:04:28 +0530

    Ready for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = 'https://fir-tracking.onrender.com';
 
 // Create axios instance
 const api = axios.create({
